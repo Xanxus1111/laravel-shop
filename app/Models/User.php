73904c44da8acf67cb026e32064a9e99e1f48a19
Xanxus1111 <809339558@qq.com>
2018-09-11 07:54:14 +0000

    邮箱验证中间件

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -14,8 +14,12 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+     protected $fillable = [
+         'name', 'email', 'password', 'email_verified',
+     ];
+
+     protected $casts = [
+        'email_verified' => 'boolean',
     ];
 
     /**
